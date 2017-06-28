de87fbb865da9c2b6056ad7c582da4a4f5288657
Sergey Bokatuk <s.bokatuk@dreamteam-mobile.com>
2017-06-28 11:56:48 +0300

    fix download script

--- build-scripts/travis-before-build.sh
+++ build-scripts/travis-before-build.sh
@@ -1,17 +1,10 @@
 #!/bin/bash
 
 #
-# Variables
-#
-export MonoVersion=5.0.1.1
-export MonoTouchVersion=10.10.0.36
-export ActivatorVersion=1.0.1
-
-#
 # Download and install Mono and Xamarin.iOS
 #
 echo Download and install Mono and Xamarin.iOS
-wget -nc -P downloads "http://download.mono-project.com/archive/${MonoVersion}/macos-10-universal/MonoFramework-MDK-${MonoVersion}.macos10.xamarin.universal.pkg"
-sudo installer -pkg "downloads/MonoFramework-MDK-${MonoVersion}.macos10.xamarin.universal.pkg" -target / 
-wget -nc -P downloads "http://download.xamarin.com/MonoTouch/Mac/monotouch-${MonoTouchVersion}.pkg"
-sudo installer -pkg "downloads/monotouch-${MonoTouchVersion}.pkg" -target /
+wget -nc -P downloads "https://download.mono-project.com/archive/5.0.1/macos-10-universal/MonoFramework-MDK-5.0.1.1.macos10.xamarin.universal.pkg"
+sudo installer -pkg "downloads/MonoFramework-MDK-5.0.1.1.macos10.xamarin.universal.pkg" -target / 
+wget -nc -P downloads "https://dl.xamarin.com/MonoTouch/Mac/xamarin.ios-10.10.0.36.pkg"
+sudo installer -pkg "downloads/xamarin.ios-10.10.0.36.pkg" -target /
